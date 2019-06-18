60196179bd7a4ae27daf0677664ed76496c6297d
Brian Salemi <briandsalemi@gmail.com>
2019-06-18 01:22:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -6,7 +6,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :image_url
       t.string :size
-      t.timestamps
+      t.timestamps :created_at
+      t.timestamps :updated_at
     end
   end
   end
