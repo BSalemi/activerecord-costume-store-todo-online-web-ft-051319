c233affd725731b4438375e398774b6af220aab0
Brian Salemi <briandsalemi@gmail.com>
2019-06-18 01:16:45 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,16 +1,16 @@
 # Create your costume_stores migration here
 
 class CreateCostumeStores << ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_stable costume_stores do |t|
-      t.string :name 
+      t.string :name
       t.string :location
-      t.integer :costume_inventory 
+      t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.start_time :opening_time
       t.end_time :closing_time
-    end 
-  end 
+    end
+  end
 
 end 
